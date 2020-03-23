fe0e2a546aa156f8b6780c761429ad51703d81de
SAGH <42285767+8sagh8@users.noreply.github.com>
2020-03-23 00:18:58 -0400

    new folder Numpy Short Codes

---
+++ Numpy_Short_Codes/One_Dimension_Array.py
@@ -0,0 +1,12 @@
+"""
+@author: Sammar Abbas
+"""
+
+import numpy as np ### defining alias of it
+
+#creating Ndarray
+a = np.array([1,2,3]) #One dimension array
+
+print(a)
+print(type(a))
+
---
+++ Numpy_Short_Codes/Two_Dimension_Array.py
@@ -0,0 +1,19 @@
+"""
+@author: Sammar Abbas
+"""
+
+import numpy as np ### defining alias of it
+
+""" for n type of array we will enclose them in LIST
+    print function will show it in n type OR in MATRIX of same rows and columns
+"""
+
+b = np.array([[4,5,6],[7,8,9]])  #two dimension array
+lst = [[4,5,6],[7,8,9]]
+
+print(b)
+print(type(b))
+
+print(lst)
+print(type(lst))
+
